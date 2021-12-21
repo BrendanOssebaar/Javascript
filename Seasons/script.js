2c96d17d1c5ba12c18482561825a8ed184e52c7a
BrendanOssebaar <34114@ma-web.nl>
2021-12-21 12:09:16 +0100

    Dinsdag 21 December

---
+++ Seasons/script.js
@@ -0,0 +1,39 @@
+const myTitle = document.getElementById('myTitle');
+const myImage = document.getElementById('myImage');
+
+let seizoenen = [
+    {
+        "titel" : "Seizoenen",
+        "image" : "img/seasons.jpg",
+        "background":"gray"
+    } ,
+    {
+        "titel" : "Lente",
+        "image" : "img/spring.jpg",
+        "background":"green"
+    } ,
+    {
+        "titel" : "Zomer",
+        "image" : "img/summer.jpg",
+        "background":"yellow"
+    } ,
+    {
+        "titel" : "Herfst",
+        "image" : "img/autumn.jpg",
+        "background":"orange"
+    } ,
+    {
+        "titel" : "Winter",
+        "image" : "img/winter.jpg",
+        "background":"white"
+    } 
+];
+
+function show(index){
+    myTitle.innerHTML = seizoenen[index].titel;
+    myImage.src = seizoenen[index].image;
+    document.body.style.backgroundColor = seizoenen[index].background;
+}
+
+
+
